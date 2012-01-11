8b574596b7982db762591ab3e8daf70b89f10e7e
Joshua Hykes <jhykes@gmail.com>
2012-01-11 09:41:18 -0500

    Starting isotope_data project.

---
+++ isotope_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+Access to isotope atomic weights and abundances.
+
+Data from NIST.
+http://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl?ele=&ascii=ascii2&isotype=all
+
+"""
+
+import numpy as np
+import uncertainties as unc
+
+
