987ca68ee2217abdf3d910ebe241fc9fd29a5641
Joshua Hykes <jhykes@gmail.com>
2012-01-14 12:50:47 -0500

    Roughing in integration of Nuclear wallet cards

--- isotope_data.py
+++ isotope_data.py
@@ -7,22 +7,13 @@ http://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl?ele=&ascii=ascii2&is
 
 """
 
+import gzip
+
 import numpy as np
+
 import uncertainties as unc
 
-# NIST data file
-data_file = "isotope_data.txt"
-
-# chunk file into isotopes
-isotope_raw_list = []
-current_isotope = []
-for line in open(data_file):
-    if line == '\n':
-        isotope_raw_list.append(current_isotope)
-        current_isotope = []
-    else:
-        current_isotope.append(line.rstrip())
-
+# NIST data -------------------------------------------------------------
 def split_line(line):
     return map(str.strip, line.split('='))
 
@@ -42,17 +33,80 @@ def parse_one_chunk(chunk):
 
     return d
 
-isotope_processed_list = []
-for raw_chunk in isotope_raw_list:
-    isotope_processed_list.append( parse_one_chunk(raw_chunk) )
+# NIST data file
+data_file = "isotope_data.txt"
+
+# chunk file into isotopes
+nist_isotope_raw_list = []
+current_isotope = []
+for line in open(data_file):
+    if line == '\n':
+        nist_isotope_raw_list.append(current_isotope)
+        current_isotope = []
+    else:
+        current_isotope.append(line.rstrip())
 
 
-per_element = {}
-for isotope in isotope_processed_list:
+nist_isotope_processed_list = []
+for raw_chunk in nist_isotope_raw_list:
+    nist_isotope_processed_list.append( parse_one_chunk(raw_chunk) )
+
+
+nist_per_element = {}
+for isotope in nist_isotope_processed_list:
     Z = isotope['Atomic Number']
     try:
-        per_element[Z].append(isotope)
+        nist_per_element[Z].append(isotope)
     except KeyError:
-        per_element[Z] = []
-        per_element[Z].append(isotope)
+        nist_per_element[Z] = []
+        nist_per_element[Z].append(isotope)
+
+
+# Nuclear wallet cards data ------------------------------
+
+def nndc_unc(string, delimiter):
+    a, b = map(float, string.split(delimiter))
+    return unc.ufloat((a,b))
+
+def do_if_present(string, func):
+    string = string.strip()
+    if string:
+        return func(string)
+    else:
+        return None
+    
+
+def parse_one_wallet_line(line):
+    d = {}
+    d['A'] = int(line[1:4])
+    d['isomeric'] = (line[4] == 'M')
+    d['Z'] = int(line[6:9])
+    d['Atomic Symbol'] = line[10:12].title()
+    d['J_pi'] = line[16:26]
+    d['decay mode'] = do_if_present(line[30:34], str)
+
+    d['branch fraction'] = do_if_present(line[34:41], lambda x: float(x) / 100.)
+
+    d['excitation energy'] = do_if_present(line[42:49], lambda x: float(x))
+
+    d['Q-value'] = float(line[49:56])                  # in MeV
+    d['half-life string'] = line[63:80]        
+
+    d['abundance'] = do_if_present(line[81:96], lambda x: nndc_unc(x,'%')/100.)
+   
+    d['atomic mass'] = unc.ufloat((line[97:105], line[105:113])) # in MeV
+    d['systematics mass'] = (line[114] == 'S')
+    d['half-life'] = float(line[124:133])   # in seconds
+
+
+wallet_filename = 'nuclear-wallet-cards.txt.gz'
+wallet_file = gzip.open(wallet_filename, 'rb')
+wallet_content = wallet_file.read()
+wallet_file.close()
+
+wallet_lines = wallet_content.split('\n')[:-1]
+
+wallet_isotope_processed_list = []
+for line in wallet_lines:
+    wallet_isotope_processed_list.append( parse_one_wallet_line(line) )
 
